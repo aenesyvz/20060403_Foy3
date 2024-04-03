1004b9cb8f0ed5219aa364f5e5e365da6fc7003c
aenesyvz <20060403@stu.omu.edu.tr>
2024-04-03 20:10:06 +0300

    fixed Turkish charachter problem

--- added_data_to_tables.sql
+++ added_data_to_tables.sql
@@ -1,18 +1,18 @@
 USE Foy3;
 --Birimler tablosu
-INSERT INTO birimler values ('1','Yazýlým')
-INSERT INTO birimler values ('2','Donaným')
-INSERT INTO birimler values ('3','Güvenlik')
+INSERT INTO birimler values ('1','YazÄ±lÄ±m')
+INSERT INTO birimler values ('2','DonanÄ±m')
+INSERT INTO birimler values ('3','GÃ¼venlik')
 
---Çalisanlar tablosu
-INSERT INTO calisanlar values ('1','Ýsmail','Ýþeri','100000','2014-02-20 00:00:00.000','1')
-INSERT INTO calisanlar values ('2','Hami','Satýlmýþ','80000','2014-06-11 00:00:00.000','1')
-INSERT INTO calisanlar values ('3','Durmuþ','Þahin','30000','2014-02-20 00:00:00.000','2')
-INSERT INTO calisanlar values ('4','Kaðan','Yazar','500000','2014-02-20 00:00:00.000','3')
-INSERT INTO calisanlar values ('5','Meryem','Soysaldý','500000','2014-06-11 00:00:00.000','3')
-INSERT INTO calisanlar values ('6','Duygu','Akþehir','200000','2014-06-11 00:00:00.000','2')
-INSERT INTO calisanlar values ('7','Kübra','Seyhan','75000','2014-01-20 00:00:00.000','1')
-INSERT INTO calisanlar values ('8','Gülcan','Yýldýz','90000','2014-04-11 00:00:00.000','3')
+--ï¿½alisanlar tablosu
+INSERT INTO calisanlar values ('1','Ä°smail','Ä°ÅŸeri','100000','2014-02-20 00:00:00.000','1')
+INSERT INTO calisanlar values ('2','Hami','SatÄ±lmÄ±ÅŸ','80000','2014-06-11 00:00:00.000','1')
+INSERT INTO calisanlar values ('3','DurmuÅŸ','Åžahin','30000','2014-02-20 00:00:00.000','2')
+INSERT INTO calisanlar values ('4','KaÄŸan','Yazar','500000','2014-02-20 00:00:00.000','3')
+INSERT INTO calisanlar values ('5','Meryem','Soysaldi','500000','2014-06-11 00:00:00.000','3')
+INSERT INTO calisanlar values ('6','Duygu','AkÅŸehir','200000','2014-06-11 00:00:00.000','2')
+INSERT INTO calisanlar values ('7','KÃ¼bra','Seyhan','75000','2014-01-20 00:00:00.000','1')
+INSERT INTO calisanlar values ('8','GÃ¼lcan','YÄ±ldÄ±z','90000','2014-04-11 00:00:00.000','3')
 
 --Ikramiye tablosu
 INSERT INTO ikramiye values ('1','5000','2016-02-20 00:00:00.000')
@@ -22,11 +22,11 @@ INSERT INTO ikramiye values ('1','4500','2016-02-20 00:00:00.000')
 INSERT INTO ikramiye values ('2','3500','2016-06-11 00:00:00.000')
  
 ----Unvan tablosu
-INSERT INTO unvan VALUES ('1', 'Yönetici', '2016-02-20 00:00:00.000')
+INSERT INTO unvan VALUES ('1', 'YÃ¶netici', '2016-02-20 00:00:00.000')
 INSERT INTO unvan VALUES ('2', 'Personel', '2016-06-11 00:00:00.000')
 INSERT INTO unvan VALUES ('8', 'Personel', '2016-02-20 00:00:00.000')
 INSERT INTO unvan VALUES ('7', 'Personel', '2016-02-20 00:00:00.000')
-INSERT INTO unvan VALUES ('5', 'Müdür', '2016-02-20 00:00:00.000')
-INSERT INTO unvan VALUES ('4', 'Personel Yardýmcýsý', '2016-06-11 00:00:00.000')
-INSERT INTO unvan VALUES ('6', 'Takým Lideri', '2016-06-11 00:00:00.000')
-INSERT INTO unvan VALUES ('3', 'Takým Lideri', '2016-02-20 00:00:00.000')
+INSERT INTO unvan VALUES ('5', 'MÃ¼dÃ¼r', '2016-02-20 00:00:00.000')
+INSERT INTO unvan VALUES ('4', 'Personel YardÄ±mcÄ±sÄ±', '2016-06-11 00:00:00.000')
+INSERT INTO unvan VALUES ('6', 'TakÄ±m Lideri', '2016-06-11 00:00:00.000')
+INSERT INTO unvan VALUES ('3', 'TakÄ±m Lideri', '2016-02-20 00:00:00.000')
